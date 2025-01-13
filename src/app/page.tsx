6061a73a008c27dcd1001535ed6cd72712b62d6f
Tina Mai <tinabmai@gmail.com>
2025-01-13 15:06:25 -0800

    more updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 			</div>
 
 			<div className="vertical justify-center items-center p-10 pt-20">
-				<div className="text-center text-2xl font-medium">Weeks of gathering receipts and creating a loss inventory reduced to minutes</div>
+				<div className="text-center text-3xl w-full max-w-screen-sm font-medium">Weeks of gathering receipts and creating a loss inventory reduced to minutes</div>
 			</div>
 			<Footer />
 		</main>
