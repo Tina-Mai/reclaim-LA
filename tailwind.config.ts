43cbf0c47c8cbf80f54198a3c4a7b0099e263529
Tina Mai <tinabmai@gmail.com>
2025-01-13 13:22:10 -0800

    format, colors, basic metadata

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				theme: {
+					green: "#DDE9D3",
+					blue: "#E5EAF2",
+				},
+			},
+			fontFamily: {
+				serif: ["Playfair Display", "serif"],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
