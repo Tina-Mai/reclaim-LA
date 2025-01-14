8a1e5b72d08ea318cdf024a3303b93a8d65b6d8c
Tina Mai <tinabmai@gmail.com>
2025-01-13 16:47:39 -0800

    image looks great

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<main>
 			{/* <ScrollButton /> */}
-			<div className="vertical min-h-[calc(100dvh)] justify-center items-center p-5">
+			<div className="vertical min-h-[calc(100dvh)] justify-center items-center">
 				<Header />
 				<div className="flex flex-col gap-10 max-w-screen-sm justify-center items-center pt-28 pb-12">
 					<h1 className="text-center text-6xl sm:text-7xl font-serif font-medium">Reclaim what you can from the LA fires</h1>
@@ -25,15 +25,17 @@ export default function Home() {
 					<UseCard title="Photos" description="Identifying belongings and finding matching items online… " icon="photos" />
 					<UseCard title="Amazon" description="Cataloging lost items from order history…" icon="amazon" />
 				</div>
-				<div className="sm:px-5 w-full">
+				<div className="w-full relative -z-10 -mt-96">
+					<div className="absolute w-full h-32 top-0 bg-gradient-to-b from-background to-transparent z-10"></div>
 					<Image src="/neighborhood.png" alt="Los Angeles Neighborhood" width={0} height={0} sizes="100vw" className="w-full h-auto rounded-[2rem]" priority />
 				</div>
 			</div>
 
-			<div className="vertical justify-center items-center p-10 pt-20">
+			<div className="vertical justify-center items-center pt-12 p-5 gap-8">
 				<div id="learn-more-target" className="font-serif text-center text-4xl w-full max-w-screen-sm font-medium">
 					Weeks of gathering receipts and creating a loss inventory reduced to minutes
 				</div>
+				<button className="bg-zinc-800 text-white text-lg px-5 py-3 rounded-full">Get Started</button>
 			</div>
 			<Footer />
 		</main>
