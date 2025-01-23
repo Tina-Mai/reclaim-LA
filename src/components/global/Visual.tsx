bc97ba818995d90811fb86000ada21aa5f0ce5f6
Tina Mai <tinabmai@gmail.com>
2025-01-23 01:41:04 -0800

    lines connecting steps in visual

--- src/components/global/Visual.tsx
+++ src/components/global/Visual.tsx
@@ -1,15 +1,17 @@
-import { Phone, FileSpreadsheet } from "lucide-react";
+import { PhoneCall, FileSpreadsheet } from "lucide-react";
 
 const Visual = () => {
 	return (
-		<div className="horizontal gap-5">
-			<div className="vertical gap-2 items-center w-48">
+		<div className="vertical sm:horizontal gap-7 items-center">
+			<div className="vertical gap-3 items-center w-40">
 				<div className="bg-theme-orange rounded-full p-4">
-					<Phone />
+					<PhoneCall />
 				</div>
 				<div className="text-sm text-zinc-500 text-center">10 minute phone call with the Reclaim AI</div>
 			</div>
-			<div className="vertical gap-2 items-center w-48">
+			<div className="hidden sm:flex h-[2px] w-24 border-b-2 border-dotted border-zinc-400" />
+			<div className="flex sm:hidden w-[2px] h-12 border-r-2 border-dotted border-zinc-400" />
+			<div className="vertical gap-3 items-center w-40">
 				<div className="bg-theme-orange rounded-full p-4">
 					<FileSpreadsheet />
 				</div>
