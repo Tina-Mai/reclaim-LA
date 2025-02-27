9d5694fd322912c2e76845f6fb967f23db6f0004
mwirtz112 <mwirtz@candidsocial.app>
2025-02-27 12:38:28 -0800

    clean branch

--- supabase/functions/send_sms/helpers/base64.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "https://denopkg.com/chiefbiiko/base64/mod.ts" {
-  export function fromUint8Array(input: Uint8Array): string;
-  export function toUint8Array(input: string): Uint8Array;
-} 
--- supabase/functions/send_sms/types/interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Sms {
-    [index: string]: string | string[] | undefined;
-    From: string;
-    To: string;
-    Body: string;
-    mediaUrl?: string[];
-  }
