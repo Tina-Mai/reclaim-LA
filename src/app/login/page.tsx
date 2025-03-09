086ddd31057067a7505ea31fd9f3b9b56fe689b6
Tina Mai <tinabmai@gmail.com>
2025-03-08 23:19:26 -0800

    login-screen styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,8 @@ import Logo from "@/components/global/Logo";
 
 function Login() {
 	return (
-		<div className="vertical items-center justify-between h-screen p-10">
+		<div className="vertical items-center justify-between h-screen p-10 relative">
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-[radial-gradient(circle_at_center,rgba(252,160,109,0.5)_0%,transparent_50%)] -z-50" />
 			<Link href="/" className="horizontal items-center gap-3 px-3 hover:opacity-70 transition-opacity duration-300">
 				<Logo />
 				<div className="text-2xl font-serif font-medium">ReclaimLA.org</div>
@@ -22,7 +23,7 @@ function Login() {
 					</Button>
 				</div>
 			</div>
-			<div className="horizontal text-zinc-500 text-center items-center gap-3">
+			<div className="horizontal text-sm text-zinc-500 text-center items-center gap-3">
 				<Link href="/privacy">Privacy Policy</Link>
 				<div className="size-1.5 bg-zinc-500" />
 				<Link href="/terms">Terms of Service</Link>
